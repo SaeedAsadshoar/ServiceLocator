0d668596e7ed4357f2b969a8738d60f007d7437c
Saeed Asad <saeedasad.kingkode@gmail.com>
2024-06-30 10:26:12 +0330

    initial commit

---
+++ Runtime/ServiceLocator/Services/ServiceLocatorGlobal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace SashUnity.ServiceLocator
+{
+    [AddComponentMenu("ServiceLocator/ServiceLocator Global")]
+    public class ServiceLocatorGlobal : Bootstrapper
+    {
+        [SerializeField] bool dontDestroyOnLoad = true;
+
+        protected override void Bootstrap()
+        {
+            Container.ConfigureAsGlobal(dontDestroyOnLoad);
+        }
+    }
+}
